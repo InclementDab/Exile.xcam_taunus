73a001a7461ca12e1966c022e6912993913d3d57
InclementDeath <tylerpaul2329@gmail.com>
2018-06-01 01:24:08 -0700

    Fixed my shitty coding
    CDAH was borked

--- scripts/CDAH.hpp
+++ scripts/CDAH.hpp
@@ -1,28 +1,28 @@
-#include "scripts\CDAH_crafting\vehicleRecipes\CDAH_Vehicle_recipies_config.hpp"
+#include "CDAH_crafting\vehicleRecipes\CDAH_Vehicle_recipies_config.hpp"
 
 class CfgCraftingRecipies
 {
-	#include "scripts\CDAH_crafting\deviceRecipes\CDAH_Device_recipies.hpp"
-	#include "scripts\CDAH_crafting\expandedCraftingRecipes\CDAH_ExpandedCrafting_config.hpp"
+	#include "CDAH_crafting\deviceRecipes\CDAH_Device_recipies.hpp"
+	#include "CDAH_crafting\expandedCraftingRecipes\CDAH_ExpandedCrafting_config.hpp"
 };
 
 class CfgExileCustomCode
 {
 	// CDAH CRAFTING Overwrite
-	ExileClient_object_item_craft =							"scripts\CDAH_crafting\logic\CDAH_ExileClient_object_item_craft.sqf"; 
-	ExileClient_gui_crafting_show =							"scripts\CDAH_crafting\logic\CDAH_ExileClient_gui_crafting_show.sqf";
-	ExileServer_object_vehicle_carefulCreateVehicle =		"scripts\CDAH_crafting\logic\CDAH_ExileServer_object_vehicle_carefulCreateVehicle.sqf";
+	ExileClient_object_item_craft =							"CDAH_crafting\logic\CDAH_ExileClient_object_item_craft.sqf"; 
+	ExileClient_gui_crafting_show =							"CDAH_crafting\logic\CDAH_ExileClient_gui_crafting_show.sqf";
+	ExileServer_object_vehicle_carefulCreateVehicle =		"CDAH_crafting\logic\CDAH_ExileServer_object_vehicle_carefulCreateVehicle.sqf";
 				
 	// CDAH Rearm Vehicles (for Vehicle rearming)
-	ExileClient_action_execute =                            "scripts\CDAH_crafting\logic\CDAH_ExileClient_action_execute.sqf";
+	ExileClient_action_execute =                            "CDAH_crafting\logic\CDAH_ExileClient_action_execute.sqf";
 };
 
 class CfgInteractionMenus
 {
-	#include "scripts\CDAH_crafting\logic\CDAH_Class_tank.hpp"
+	#include "CDAH_crafting\logic\CDAH_Class_tank.hpp"
 };
 
 class CfgInteractionModels
 {
-	#include "scripts\CDAH_crafting\logic\CDAH_interactionModels.hpp"
+	#include "CDAH_crafting\logic\CDAH_interactionModels.hpp"
 };
